3132c9046515e7b561a95fc0673e4082d17b8412
Imad Shah <shahimad499@gmail.com>
2024-05-03 10:26:02 -0700

    images change for deploying

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -32,6 +32,8 @@ const TopBar = () => {
         <Image
           src="https://s3-alpha-sig.figma.com/img/ae0a/ba3f/e813d9151880c1b698c7f8d68d179cc3?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=IYLjPT8K9t4nLNAHK-JhC5NJlnCbLp9TCMziSBDkZR3kr6JBWOGT9PLjONlymO-8jr9fPrVeRkFDehCTWCk72tIzPFfQCxy-kUeMMv8iHx7flG1xQMveAhF8CwDAE2IEAAHdXm~DCgfjYgbZrS2lUgBJwMXzBSN5l4nCucSCSz-5CbL6WGpx5CPmBDahY5cIiy2eew--NW8R-Qy7m6tRa66Fnic4yQsTc9KbR1nPPSyTQId~EyUBr~hELdTJtJoTc~fdmFX75OJg4FFOjiGDE6ZoSrq~9D0FerHT1VYZdkAnnJoNLSIUQgIIKER~LRhf3dd~TXAbAXXEfG-YGZ2oGw__"
           alt="logo"
+          width={50}
+          height={50}
           style={{ width: "50px", height: "50px" }}
         />
         <a href="#">CUSTOMERS</a>
